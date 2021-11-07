eeeb85c6675a1d249d96a30267c1874606092c43
GXT <test@MacBook.local>
2021-11-07 15:55:19 +0900

    adding valuables

---
+++ script.js
@@ -0,0 +1,16 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "programmer";
+let mySecondJob = "teacher";
+
+console.log(myFirstJob);
