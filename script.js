8617681e698cac25b29439a9c19f664f26c223f1
GXT <test@MacBook.local>
2021-11-10 23:29:50 +0900

    adding Assignment

--- script.js
+++ script.js
@@ -60,6 +60,7 @@ lastName = "Schmedtmann";
 console.log(lastName);
 */
 
+//Math operator
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
@@ -71,3 +72,19 @@ console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
 const firstName = "Jonas";
 const lastName = "Schmedtmann";
 console.log(firstName + " " + lastName);
+
+//Assignment operators
+let x = 10 + 5; //15
+x += 10; //x = x + 10
+x *= 4; //x = x * 4 =100
+x++; //x = x + 1
+x--; //x = x - 1
+console.log(x);
+
+//Comparison operators
+console.log(ageJonas > ageSarah); //>, <,>=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
