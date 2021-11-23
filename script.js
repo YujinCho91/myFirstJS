a69032da2a0aade42211a107137e4adc00b36c61
GXT <test@MacBook.local>
2021-11-23 23:15:43 +0900

    adding conditional operator

--- script.js
+++ script.js
@@ -287,12 +287,12 @@ if (height) {
 
 // if (favorite !== 23) console.log("Why not 23?");
 
-const hasDriversLicense = true; //A
-const hasGoodVision = true; //B
+// const hasDriversLicense = true; //A
+// const hasGoodVision = true; //B
 
-console.log(hasDriversLicense && hasGoodVision);
-console.log(hasDriversLicense || hasGoodVision);
-console.log(!hasDriversLicense);
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense);
 
 // const shouldDrive = hasDriversLicense && hasGoodVision;
 
@@ -302,15 +302,15 @@ console.log(!hasDriversLicense);
 //   console.log("Someone else should drive...");
 // }
 
-const isTired = true; //C
-console.log(hasDriversLicense || hasGoodVision || isTired);
-console.log(hasDriversLicense && hasGoodVision && isTired);
+// const isTired = true; //C
+// console.log(hasDriversLicense || hasGoodVision || isTired);
+// console.log(hasDriversLicense && hasGoodVision && isTired);
 
-if (hasDriversLicense && hasGoodVision && !isTired) {
-  console.log("Sarah is able to drive!");
-} else {
-  console.log("Someone else should drive...");
-}
+// if (hasDriversLicense && hasGoodVision && !isTired) {
+//   console.log("Sarah is able to drive!");
+// } else {
+//   console.log("Someone else should drive...");
+// }
 
 /*
 There are two gymnastics teams, Dolphins and Koalas. They compete against each
@@ -377,42 +377,99 @@ Test data:
 //   console.log("No one wins the trophy");
 // }
 
-const day = "tuesday";
+// const day = "tuesday";
 
-switch (day) {
-  case "monday": // day==='monday'
-    console.log("Plan course structure");
-    console.log("Go to coding meetup");
-    break;
-  case "tuesday": // day==='tuesday'
-    console.log("Prepare theory videos");
-    break;
-  case "wednesday":
-  case "thursday":
-    console.log("Write code examples");
-    break;
-  case "friday":
-    console.log("Record videos");
-    break;
-  case "saturday":
-  case "sunday":
-    console.log("Enjoy the weekend :D");
-    break;
-  default:
-    console.log("Not a valid day!");
-}
+// switch (day) {
+//   case "monday": // day==='monday'
+//     console.log("Plan course structure");
+//     console.log("Go to coding meetup");
+//     break;
+//   case "tuesday": // day==='tuesday'
+//     console.log("Prepare theory videos");
+//     break;
+//   case "wednesday":
+//   case "thursday":
+//     console.log("Write code examples");
+//     break;
+//   case "friday":
+//     console.log("Record videos");
+//     break;
+//   case "saturday":
+//   case "sunday":
+//     console.log("Enjoy the weekend :D");
+//     break;
+//   default:
+//     console.log("Not a valid day!");
+// }
 
-if (day === "monday") {
-  console.log("Plan course structure");
-  console.log("Go to coding meetup");
-} else if (day === "tuesday") {
-  console.log("Prepare theory videos");
-} else if (day === "wednesday" || day === "thursday") {
-  console.log("Write code examples");
-} else if (day === "friday") {
-  console.log("Record videos");
-} else if (day === "saturday" || day === "sunday") {
-  ("Enjoy the weekend :D");
-} else {
-  console.log("Not a valid day!");
-}
+// if (day === "monday") {
+//   console.log("Plan course structure");
+//   console.log("Go to coding meetup");
+// } else if (day === "tuesday") {
+//   console.log("Prepare theory videos");
+// } else if (day === "wednesday" || day === "thursday") {
+//   console.log("Write code examples");
+// } else if (day === "friday") {
+//   console.log("Record videos");
+// } else if (day === "saturday" || day === "sunday") {
+//   ("Enjoy the weekend :D");
+// } else {
+//   console.log("Not a valid day!");
+// }
+
+// 3 + 4;
+// 1991;
+// true && false && !false;
+
+// if (23 > 10) {
+//   const str = "23 is bigger";
+// }
+
+// const me = "Jonas";
+// console.log(`I'm ${2027 - 1991} yeears old ${me}`);
+
+// const age = 23;
+// age >= 18
+//   ? console.log("I like to drink wine🍷")
+//   : console.log("I like to drink water🧋");
+
+// const drink = age >= 18 ? "wine🍷" : "water🧋";
+// console.log(drink);
+
+// let drink2;
+// if (age >= 18) {
+//   drink2 = "wine🍷";
+// } else {
+//   drink2 = "water🧋";
+// }
+// console.log(drink2);
+
+// console.log(`I like to drink ${age >= 18 ? "wine🍷" : "water🧋"}`);
+
+// Steven wants to build a very simple tip calculator for whenever he goes eating in a
+// restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
+// 300. If the value is different, the tip is 20%.
+
+// Your tasks:
+// 1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for
+// this. It's not allowed to use an if/else statement �
+// (If it's easier for you, you can
+// start with an if/else statement, and then try to convert it to a ternary
+// operator!)
+
+// 2. Print a string to the console containing the bill value, the tip, and the final value
+// (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value
+// 316.25”
+
+// Test data:
+// § Data 1: Test for bill values 275, 40 and 430
+// Hints:
+// § To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+// § Value X is between 50 and 300, if it's >= 50 && <= 300 �
+
+const bill = 275;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+);
